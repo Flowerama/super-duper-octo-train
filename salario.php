346e978e4e5fed8ee515903f4b5cdccb01f70a23
Nicole Gabriele Gomes <48127783+Flowerama@users.noreply.github.com>
2019-03-01 11:41:58 -0300

    Update salario.php

--- salario.php
+++ salario.php
@@ -10,4 +10,7 @@ $porcentual = (int) fgets (STDIN);
 
 $aumento_do_salario = $salario_sem_aumento * $porcentual;
 
-print "o seu salário com aumento é $aumento_do_salario reais."
+$salario_com_aumento = $salario_sem_aumento + $porcentual;
+
+print "o seu salário com aumento é $aumento_com_salario reais."
+)
