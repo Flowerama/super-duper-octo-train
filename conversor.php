505e9bf56a03d5eb7a2a4d0aa49b4c2ffebb28f9
Nicole Gabriele Gomes <48127783+Flowerama@users.noreply.github.com>
2019-03-01 10:57:52 -0300

    Create conversor.php

---
+++ conversor.php
@@ -0,0 +1,25 @@
+<?php
+
+print "Qual a quantidade em metros a ser convertidos?";
+
+$quantidade_metros = (int)fgets (STDIN);
+
+$quantidade_cm = $quantidade_metros * 100;
+<?php
+
+print "Qual a quantidade em metros a ser convertidos?";
+
+$quantidade_metros = (int)fgets (STDIN);
+
+$quantidade_cm = $quantidade_metros * 100;
+
+print "A quantidade de $quantidade_metros foi convertida para $quantidade_cm";
+print "A quantidade de $quantidade_metros foi convertida para $quantidade_cm";<?php
+
+print "Qual a quantidade em metros a ser convertidos?";
+
+$quantidade_metros = (int)fgets (STDIN);
+
+$quantidade_cm = $quantidade_metros * 100;
+
+print "A quantidade de $quantidade_metros foi convertida para $quantidade_cm";
