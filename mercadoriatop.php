34a9a6d54975a9d4be2596b61b1b957f6277c17b
Nicole Gabriele Gomes <48127783+Flowerama@users.noreply.github.com>
2019-03-14 08:31:52 -0300

    Update and rename mercadoria.php to mercadoriatop.php
    Maria fez esse tb

---
+++ mercadoriatop.php
@@ -0,0 +1,24 @@
+<?php
+
+    print("Qual o valor da mercadoria: ");
+    $valor = (INT) fgets(STDIN);
+
+    $valortotal = $valor *0.90;
+
+    print("O valor de desconto é de 10% e o valor total é de $valortotal \n");
+<?php
+
+    print("Qual o valor da mercadoria: ");
+    $valor = (INT) fgets(STDIN);
+
+    $valortotal = $valor *0.90;
+
+    print("O valor de desconto é de 10% e o valor total é de $valortotal \n");
+<?php
+
+    print("Qual o valor da mercadoria: ");
+    $valor = (INT) fgets(STDIN);
+
+    $valortotal = $valor *0.90;
+
+    print("O valor de desconto é de 10% e o valor total é de $valortotal \n");
